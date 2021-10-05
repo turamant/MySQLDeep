503c56735f77acf593ce73981b58108ffe325356
askvart <tur1amant@gmail.com>
2021-10-05 22:15:49 +0300

    l2

---
+++ p1_create_connect.py
@@ -0,0 +1,51 @@
+import sys
+import mysql.connector
+
+try:
+    db1 = mysql.connector.connect(
+        option_files="config.ini")
+
+    # db1 = mysql.connector.MySQLConnection(
+    #    **connect_args
+    #    )
+
+    # db1 = mysql.connector.MySQLConnection()
+    # db1.connect(**connect_args)
+
+    # db1 = mysql.connector.MySQLConnection()
+    # db1.config(**connect_args)
+    # db1.connect()
+
+    print("Успешное соединение с БД ")
+    print("MySQL connection ID for db2: {0}".format(db1.connection_id))
+
+except:
+    print("Connection with Base Data error!")
+    sys.exit(1)
+cursor = db1.cursor()
+try:
+    cursor.execute("SELECT ID, Name from city")
+    print("ID\tName")
+    while True:
+        row = cursor.fetchone()
+        if row == None:
+            break
+        print("%d\t\t\t%s" % (row[0], row[1]))
+except:
+    print("error in fetching(выборка) rows")
+    # sys.exit(1)
+
+print("Second variant")
+try:
+    cursor.execute("SELECT ID, Name from city")
+    print("ID\tName")
+    rows = cursor.fetchall()
+    for row in rows:
+        print("%d\t\t\t%s" % (row[0], row[1]))
+except:
+    print("Error fetching rows")
+    # sys.exit(1)
+
+cursor.close()
+print("MySQL connection ID for db2: {0}".format(db1.connection_id))
+db1.close()
---
+++ p2_cmd_query.py
@@ -0,0 +1,23 @@
+import sys
+import mysql.connector
+import pprint
+
+printer = pprint.PrettyPrinter(indent=1)
+
+try:
+    db = mysql.connector.connect(
+        option_files="config.ini")
+except:
+    print("Connection with Base Data error!")
+
+# Execute a query
+
+result = db.cmd_query(
+    """SELECT *
+    FROM world.city
+    WHERE ID = 130"""
+)
+
+print("Result Dictionary\n" + "=" * 17)
+printer.pprint(result)
+db.close()
