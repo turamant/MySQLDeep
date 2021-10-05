2506e3571f7db94d32e6ee9781a260f8b91a7100
askvart <tur1amant@gmail.com>
2021-10-05 22:59:42 +0300

    new file

---
+++ p3_cursor_execute_fetch_one.py
@@ -0,0 +1,35 @@
+import mysql.connector
+
+# Create connection to MySQL
+db = mysql.connector.connect(
+    option_files="config.ini")
+
+# Instantiate the cursor
+cursor = db.cursor()
+
+# Execute the query
+cursor.execute("""SELECT Name, CountryCode,
+                Population
+                FROM world.city
+                WHERE Population > 9000000
+                ORDER BY Population DESC"""
+                )
+
+print(__file__ + " - Using the default cursor:")
+print("")
+if cursor.with_rows:
+    print("{0:15s}{1:8s}{2:5s}".format(
+            "City", "Country", "Pop"
+        )
+    )
+city = cursor.fetchone()
+while city:
+    print("{0:15s}{1:^7s}{2:4.1f}".format(
+            city[0],
+            city[1],
+            city[2]/1000000.0
+        )
+    )
+    city = cursor.fetchone()
+cursor.close()
+db.close()
